c7b970be8621a772ec41b179432ef7c8221ff96b
space <space@spacedeiMac.local>
2015-04-01 11:52:18 +0800

    Initial Commit

---
+++ 动画/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  动画
+//
+//  Created by space on 15/4/1.
+//  Copyright (c) 2015年 ZYahurey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 动画/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  动画
+//
+//  Created by space on 15/4/1.
+//  Copyright (c) 2015年 ZYahurey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
